49e4cd392835ee0e07ecc40ad5a573e9c509d69f
Dmitry Void <supsad@yahoo.com>
2023-10-31 22:38:09 +0700

    Second Homework ! TO_TRANSFER ! "Генерация данных"
    1. main.js
    * added CONST variables
    * united random functions via switch case
    * added 2 functions which worked of arrays - in progress
    * added 3 functions which create objects - in progress
    * added function which create 1 array of 10 objects in elements
    * added function which get mask of location (x, y) - in progress
    
    2. *.js
    * other js files are added later for next homework // Uncaught SyntaxError: import declarations may only appear at top level of a module
    
    3. index.html
    * added 2 new script for modules for next homework // commented

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
-'use strict'
+'use strict';
 
-// TODO Decomposing to 'switch'
-const getRandomIntInclusive = (min, max) => {
+const TITLE_OFFER = ['Hotel', 'Hostel', 'Apartment', 'Houseroom', 'Motel', 'B&B-hotel', 'Apart-hotel',
+  'Capsule hotel', 'Guest-house', 'Bed and Breakfast'];
+const FEATURES_OFFER = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const TYPE_HOUSING_OFFER = ['palace', 'flat', 'house', 'bungalow'];
+const SIMILAR_ADVERTISEMENTS_COUNT = 10;
+
+const getRandomInclusive = (min, max, numberType = 'int', decimals = 5) => {
   if (min < 0 || max < 0) {
     return -1;
   }
@@ -10,28 +15,56 @@ const getRandomIntInclusive = (min, max) => {
     [min, max] = [max, min];
   }
 
-  min = Math.ceil(min);
-  max = Math.floor(max);
+  switch (numberType) {
+    case 'int':
+      min = Math.ceil(min);
+      max = Math.floor(max);
 
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+
+    // * .toFixed() at the end because in .parseFloat() it didn't produce fixed length decimals
+    case 'float':
+    case 'double':
+      return parseFloat((Math.random() * (max - min) + min)).toFixed(decimals);
+
+    default:
+      return -1;
+  }
 };
 
-getRandomIntInclusive(0, 100);
+const shuffleArray = (array) => {
+  let currentIndex = array.length;
 
-const getRandomFloatInclusive = (min, max) => {
-  if (min < 0 || max < 0) {
-    return -1;
+  while (0 !== currentIndex) {
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    let temp = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temp;
   }
 
-  if (max < min) {
-    [min, max] = [max, min];
-  }
-
-  return Number((Math.random() * (max - min + 1) + min).toFixed(1));
+  return array;
 }
 
+// ! Возможно, сделать через new Array ? (унифицированную функцию)
+const createNumbersArray = (length) => {
+  let arr = [];
 
-getRandomFloatInclusive(0, 100);
+  for (let i = 1; i <= length; i++) {
+    arr.push(i);
+  }
+
+  console.log(arr);
+  return arr;
+}
+
+// TODO из-за того, что вызов функции каждый раз новый, простой цикл не подойдет. Придется либо глобальную переменную
+// использовать, либо пересоздавать массив, удаляю из него полученный значения
+
+createNumbersArray(10);
+let arr = shuffleArray(createNumbersArray(10));
+console.log(arr);
 
 // TODO Homework Chapter 3
 // ! Creating an array of 10 generated JS objects
@@ -66,3 +99,61 @@ location (obj) = {
   y (float): n.m(5), // random float from 139.70000 to 139.80000
 };
 */
+
+const createAuthorAvatar = () => {
+  const author = {
+    // TODO uniq number (create array with exit indexes)
+    avatar: `img/avatars/user0${getRandomInclusive(0, 10, 'int')}.png`,
+  };
+
+  return {
+    author,
+  };
+};
+
+// ! Адреса и локации разнятся, нужно куда-то сохранять значения
+// TODO use destructuring to 'x' and 'y' arguments
+const createLocation = () => {
+  const location = {
+    x: getRandomInclusive(35.65000, 35.70000, 'float', 5),
+    y: getRandomInclusive(139.70000, 139.80000, 'float', 5),
+  };
+
+  return {
+    location,
+  };
+};
+
+const getLocationMask = (obj) => {
+  return Object.entries(obj.location)
+    .map(([key, value]) => `${key}.${value}`)
+    .join(', ');
+};
+
+const createOffer = () => {
+  const offer = {
+    title: 'hotel', // TODO data.js - [hotel, hostel, сделать массив с названиями отелей]
+    address: getLocationMask(createLocation()), // TODO чтобы значения сохранялись в объекте
+    price: getRandomInclusive(10_000, 1_000_000, 'int'),
+    type: 'one of four', // TODO data.js - fixed values - [palace, flat, house, bungalow],
+    rooms: getRandomInclusive(1, 6, 'int'),
+    guests: getRandomInclusive(1, 10, 'int'),
+    checkin: `${getRandomInclusive(12, 14, 'int')}:00`,
+    checkout: `${getRandomInclusive(12, 14, 'int')}:00`,
+    features: ['string'], // TODO unique arr random length - [wifi, dishwasher, parking, washer, elevator, conditioner]
+    description: '?for pidors!',
+    photos: ['string'],
+  };
+
+  return {
+    offer,
+  };
+};
+
+const createFullOfferNearby = () => Object.assign({}, createAuthorAvatar(), createLocation(), createOffer());
+
+const similarAdvertisementsNearby = new Array(SIMILAR_ADVERTISEMENTS_COUNT)
+  .fill(null)
+  .map(() => createFullOfferNearby());
+
+console.log(similarAdvertisementsNearby);
