7c252a240c69b2d9560655531e4d89b462de12e8
Dmitry Void <supsad@yahoo.com>
2023-12-08 00:35:41 +0700

    Sixth Homework "Help from a friend" #1 / "Помощь друга" #1
    PART 4
    
    Implement main map pin and coordinate setting
    
    Added a main pin marker to the map at the coordinates representing Tokyo, and made the marker draggable. The #address field is now read-only (the value was sent to the server in the form) and is automatically populated with the current coordinates of the main pin marker whenever it is moved. This change enhances UX by allowing users to visually select a location on the map.

--- js/map/map.js
+++ js/map/map.js
@@ -4,9 +4,14 @@ import {forms, renderInactiveInterface, renderInteractiveElements} from '../inte
 
 const mapCanvas = document.querySelector('#map-canvas');
 
+const address = document.querySelector('#address');
+
 const onMapLoad = () => {
   document.removeEventListener('DOMContentLoaded', renderInactiveInterface);
   renderInteractiveElements('active', forms);
+
+  address.readOnly = true;
+  address.value = `${TokyoCoordinates.LatitudeX}, ${TokyoCoordinates.LongitudeY}`
 };
 
 const map = L.map('map-canvas')
@@ -16,13 +21,39 @@ const map = L.map('map-canvas')
     lng: TokyoCoordinates.LongitudeY,
   }, 10);
 
-L.tileLayer(
+const mapTile = L.tileLayer(
   'https://tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}',
   {
     foo: 'bar',
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   },
-).addTo(map);
+);
+
+mapTile.addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: '/img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const mainPinMarker = L.marker(
+  {
+    lat: TokyoCoordinates.LatitudeX,
+    lng: TokyoCoordinates.LongitudeY,
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon,
+  },
+);
+
+mainPinMarker.addTo(map);
+
+mainPinMarker.on('moveend', (evt) => {
+  const { lat, lng } = evt.target.getLatLng();
+  address.value = `${lat.toFixed(5)}, ${lng.toFixed(5)}`;
+});
 
 const addCardToMap = () => mapCanvas.appendChild(renderCards());
 
