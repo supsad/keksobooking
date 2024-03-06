81edacbb02e40f6afcef3b5271761e5c77d0cffa
Dmitry Void <supsad@yahoo.com>
2024-03-07 01:14:14 +0700

    Refactoring. Improved readability, divided into functions

--- js/render-page/error.js
+++ js/render-page/error.js
@@ -1,6 +1,6 @@
-const ErrorBlockTemplate = {
-  'CLASS_WRAPPER': 'api-error-container',
-  'WRAPPER_STYLE': {
+const ErrorWrapperTemplate = {
+  CLASS: 'api-error-container',
+  STYLES: {
     position: 'sticky',
     top: 0,
     left: 0,
@@ -18,44 +18,56 @@ const ErrorBlockTemplate = {
     color: 'black',
     'background-color': '#E52B50',
   },
-  'CLASS_MESSAGE': 'api-error-message',
-  'DELETE_MESSAGE_TIMER': 15000, // * 15sec
 };
 
-const getAlertStyles = (ErrorTemplate) => {
+const MESSAGE_CLASS = 'api-error-message';
+const DELETE_MESSAGE_TIMER = 15000; // * 15sec
+
+const getAlertStyles = (WrapperTemplate) => {
   return `
-    position: ${ErrorTemplate.WRAPPER_STYLE.position};
-    top: ${ErrorTemplate.WRAPPER_STYLE.top};
-    left: ${ErrorTemplate.WRAPPER_STYLE.left};
-    z-index: ${ErrorTemplate.WRAPPER_STYLE['z-index']};
+    position: ${WrapperTemplate.WRAPPER_STYLE.position};
+    top: ${WrapperTemplate.WRAPPER_STYLE.top};
+    left: ${WrapperTemplate.WRAPPER_STYLE.left};
+    z-index: ${WrapperTemplate.WRAPPER_STYLE['z-index']};
 
-    display: ${ErrorTemplate.WRAPPER_STYLE.flex};
-    justify-content: ${ErrorTemplate.WRAPPER_STYLE['justify-content']};
-    width: ${ErrorTemplate.WRAPPER_STYLE.width};
-    min-height: ${ErrorTemplate.WRAPPER_STYLE['min-height']};
-    padding: ${ErrorTemplate.WRAPPER_STYLE.padding};
-    text-align: ${ErrorTemplate.WRAPPER_STYLE['text-align']};
+    display: ${WrapperTemplate.WRAPPER_STYLE.flex};
+    justify-content: ${WrapperTemplate.WRAPPER_STYLE['justify-content']};
+    width: ${WrapperTemplate.WRAPPER_STYLE.width};
+    min-height: ${WrapperTemplate.WRAPPER_STYLE['min-height']};
+    padding: ${WrapperTemplate.WRAPPER_STYLE.padding};
+    text-align: ${WrapperTemplate.WRAPPER_STYLE['text-align']};
 
-    font-family: ${ErrorTemplate.WRAPPER_STYLE['font-family']};
-    font-size: ${ErrorTemplate.WRAPPER_STYLE['font-size']};
-    line-height: ${ErrorTemplate.WRAPPER_STYLE['line-height']};
-    font-weight: ${ErrorTemplate.WRAPPER_STYLE['font-weight']};
-    color: ${ErrorTemplate.WRAPPER_STYLE.color};
+    font-family: ${WrapperTemplate.WRAPPER_STYLE['font-family']};
+    font-size: ${WrapperTemplate.WRAPPER_STYLE['font-size']};
+    line-height: ${WrapperTemplate.WRAPPER_STYLE['line-height']};
+    font-weight: ${WrapperTemplate.WRAPPER_STYLE['font-weight']};
+    color: ${WrapperTemplate.WRAPPER_STYLE.color};
 
-    background-color: ${ErrorTemplate.WRAPPER_STYLE['background-color']};
+    background-color: ${WrapperTemplate.WRAPPER_STYLE['background-color']};
   `;
+};
+
+const getMessageWrapper = (template) => {
+  const wrapper = document.createElement('div');
+  wrapper.className = template.CLASS;
+  wrapper.style.cssText = getAlertStyles(template);
+
+  return wrapper;
+}
+
+const getMessage = (message, blockClass) => {
+  const paragraph = document.createElement('p');
+  paragraph.className = blockClass;
+  paragraph.textContent = message;
+
+  return paragraph;
 };
 
 const renderErrorAlert = (message) => {
   const container = document.querySelector('body');
 
-  const errorWrapper = document.createElement('div');
-  errorWrapper.className = ErrorBlockTemplate.CLASS_WRAPPER;
-  errorWrapper.style.cssText = getAlertStyles(ErrorBlockTemplate);
-
-  const errorMessage = document.createElement('p');
-  errorMessage.className = ErrorBlockTemplate.CLASS_MESSAGE;
-  errorMessage.textContent = message;
+  const errorWrapper = getMessageWrapper(ErrorWrapperTemplate);
+  const errorMessage = getMessage(message, MESSAGE_CLASS);
 
   errorWrapper.appendChild(errorMessage);
   container.insertAdjacentElement('afterbegin', errorWrapper);
@@ -64,7 +76,7 @@ const renderErrorAlert = (message) => {
 
   setTimeout(() => {
     container.parentElement.removeChild(errorWrapper);
-  }, ErrorBlockTemplate.DELETE_MESSAGE_TIMER);
+  }, DELETE_MESSAGE_TIMER);
 }
 
 export {renderErrorAlert}
