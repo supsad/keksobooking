60a8f3496d9ff26f6d802bdee466d4a49c04f75e
Dmitry Void <supsad@yahoo.com>
2024-03-19 01:40:19 +0700

    Translated all imports through an index of files into modules. Added script for sending data to the server

--- js/form/index.js
+++ js/form/index.js
@@ -1,11 +1,30 @@
 import {syncRoomsCapacityHandler} from './input-sync.js';
-import {syncOptionsHandler} from '/js/util.js';
+import {sendUserAnnouncementData} from '/js/api.js';
+import {page, renderErrorAlert} from '/js/render-page/index.js';
+import {getFormatTitleValue, syncOptionsHandler} from '/js/util.js';
 
-const titleInput = document.querySelector('#title');
-const timeinSelect = document.querySelector('#timein');
-const timeoutSelect = document.querySelector('#timeout');
-const roomNumberSelect = document.querySelector('#room_number');
-const capacitySelect = document.querySelector('#capacity');
+const INVALID_FOCUS = `${2}px solid #E52B50`;
+
+const ERROR_MESSAGE = [
+  'Упс! Что-то пошло не так!',
+  'Не удалось отправить ваше объявление!',
+  'Попробуйте перезагрузить страницу.',
+];
+
+const Forms = {
+  form: document.querySelector('.ad-form'),
+  filters: document.querySelector('.map__filters'),
+};
+
+const titleInput = Forms.form.querySelector('#title');
+const address = document.querySelector('#address');
+const timeinSelect = Forms.form.querySelector('#timein');
+const timeoutSelect = Forms.form.querySelector('#timeout');
+const roomNumberSelect = Forms.form.querySelector('#room_number');
+const capacitySelect = Forms.form.querySelector('#capacity');
+const houseTypeSelect = Forms.form.querySelector('#type');
+const housePriceInput = Forms.form.querySelector('#price');
+const resetFormButton = Forms.form.querySelector('button[type=reset]');
 
 const onSync = (evt) => {
   switch (evt.target) {
@@ -24,15 +43,34 @@ const onSync = (evt) => {
   }
 };
 
+const setUserFormSubmit = (onSuccess) => {
+  Forms.form.addEventListener('submit', async (evt) => {
+    evt.preventDefault();
+
+    titleInput.value = getFormatTitleValue(titleInput);
+    await sendUserAnnouncementData(
+      new FormData(evt.target),
+      () => onSuccess(),
+      () => renderErrorAlert(page, ERROR_MESSAGE),
+    );
+  });
+};
+
 export {
+  INVALID_FOCUS,
+  Forms,
+  address,
   timeinSelect,
   timeoutSelect,
   roomNumberSelect,
   capacitySelect,
   titleInput,
-  onSync
+  houseTypeSelect,
+  housePriceInput,
+  resetFormButton,
+  onSync,
+  setUserFormSubmit
 };
-
-export {onValidTitle} from './validate-title.js';
-export {syncRoomsCapacityHandler} from './input-sync.js'
-export {typeElement as houseType, syncValue as syncPrice} from './housing-type.js';
+export {onValidTitle, onValidPrice, onValidCapacity} from './validation.js';
+export {syncRoomsCapacityHandler, houseSyncPrice, setPriceInputAttr, setAddressInput} from './input-sync.js';
+export {resetPage} from './reset.js';
