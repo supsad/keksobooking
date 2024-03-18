ce252109e1272c17341f9f5b57d57aa0721d8b32
Dmitry Void <supsad@yahoo.com>
2024-03-19 01:40:51 +0700

    Added a script to reset the page to the default state

--- js/form/reset.js
+++ js/form/reset.js
@@ -1,1 +1,15 @@
-// * Blank for the project structure
+import {Forms, setAddressInput} from './index.js';
+import {setSyncForm} from '/js/render-page/index.js';
+import {Coordinates} from '/js/map/index.js';
+
+const resetPage = (mainPin) => {
+  Forms.form.reset();
+  Forms.filters.reset();
+  setSyncForm();
+
+  const coordinates = Object.values(Coordinates.Tokyo);
+  mainPin.setLatLng(coordinates);
+  setAddressInput(coordinates);
+};
+
+export {resetPage};
